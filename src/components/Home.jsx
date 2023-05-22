4cbe0d26d80a9159140d034869c3d97b8f9135e7
awadh <awadhhassan566@gmail.com>
2023-05-22 21:26:27 +0300

    🎨 addition of staete

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import mine from '../assets/mine.jpeg';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+
+const Home = () => {
+    
+  return (
+    <div name='home'
+    className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 ">
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'> I'm a Full Stack Developer</h2>
+                <p className='text-gray-500 py-4 max-w-md'>i love to work on web application using technologies like react,Tailwind,Next js and GraphQL.</p>
+                <div>
+                    <button className='text-white w-fit px-6 py-3 my-3 flex items-center rounded-md bg-gradient-to-r
+                     from-cyan-500 to-blue-500 cursor-pointer'>
+                        portfolio 
+                            <span className='hover:rotate-90 duration-300'>
+                            <MdOutlineKeyboardArrowRight />
+
+                            </span>
+                        
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img src ={mine} alt='my profile' className='rounded-2xl mx-auto
+                w=2/3 md:w-full'/>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Home
