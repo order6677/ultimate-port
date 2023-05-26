3731d785e92c81d5406a38263a4dabe1519fc594
awadh <awadhhassan566@gmail.com>
2023-05-26 12:48:35 +0300

    routing  about and  porftfolio data for the specified route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Sociallinks from "./components/Sociallinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
    <div>
     <NavBar  />
     <Home />
-    <Sociallinks />
+    <About />
+    <Sociallinks /> 
+    <Portfolio />
    </div>
   );
 }
